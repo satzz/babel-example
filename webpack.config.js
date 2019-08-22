be499842606ca33b871d5f034b41ca3769fc895c
satzz <satzz21@gmail.com>
2019-08-22 22:21:27 +0900

    use name for output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/index.js',
+  entry: {
+    index: './lib/index.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   }
 };
